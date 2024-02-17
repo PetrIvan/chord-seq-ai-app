101462635b30062120533c1fe01975a772b3d54d
PetrIvan <petr.ivan.junior@gmail.com>
2024-02-17 13:14:23 +0100

    Added Open Graph metadata
    The Open Graph title was changed to be more understandable while the description stayed the same. A new image was added, which is the same as will be the thumbnail of the product launch video.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,12 @@ export const metadata: Metadata = {
     "Compose beautiful chord progressions in your browser, with the help of AI, for free. Open-source project, code available on GitHub.",
   manifest: "/manifest.json",
   icons: { icon: "/icon-512x512.png", apple: "/icon-192x192.png" },
+  openGraph: {
+    title: "ChordSeqAI: Your AI-Powered Chord Progression Copilot",
+    description:
+      "Compose beautiful chord progressions in your browser, with the help of AI, for free. Open-source project, code available on GitHub.",
+    images: [{ url: "/og.png" }],
+  },
 };
 
 const openSans = Open_Sans({
