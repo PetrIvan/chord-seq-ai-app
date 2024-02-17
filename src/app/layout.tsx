7df8fafca73591de832e81c2958e384d7e0f246f
PetrIvan <petr.ivan.junior@gmail.com>
2024-02-17 12:57:18 +0100

    Changed title, added Apple touch icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,11 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "ChordSeqAI",
+  title: "ChordSeqAI: Your Open-Source Chord Progression AI Copilot",
   description:
     "Compose beautiful chord progressions in your browser, with the help of AI, for free. Open-source project, code available on GitHub.",
   manifest: "/manifest.json",
-  icons: { icon: "/icon-512x512.png" },
+  icons: { icon: "/icon-512x512.png", apple: "/icon-192x192.png" },
 };
 
 const openSans = Open_Sans({
