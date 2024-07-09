20c82fb31390ae7920b4e096df6bed17d11c85f5
PetrIvan <petr.ivan.junior@gmail.com>
2024-07-09 17:56:23 +0200

    Added Umami events
    Tracks when a user clicks on a support button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import QuickButtons from "@/components/landing_page/quick_buttons";
 import SupportUsButton from "@/components/landing_page/support_us_button";
 import SupportUsOverlay from "@/components/landing_page/support_us_overlay";
+import SupportUsText from "@/components/landing_page/support_us_text";
 
 export default function Home() {
   return (
@@ -225,28 +226,7 @@ export default function Home() {
             </p>
 
             {/* Only visible on mobile devices */}
-            <p className="md:hidden text-lg md:text-xl text-zinc-300 pb-5">
-              If you want to support the development of this app, you can do so
-              on{" "}
-              <a
-                className="text-blue-400 hover:underline"
-                href="https://patreon.com/xenomuse"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Patreon
-              </a>
-              , or if you prefer a one-time donation, you can use{" "}
-              <a
-                className="text-blue-400 hover:underline"
-                href="https://paypal.me/xenomuse"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                PayPal
-              </a>
-              .
-            </p>
+            <SupportUsText />
           </div>
         </div>
       </div>
